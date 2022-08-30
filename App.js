eb2c6715e3ab46fd7e137b5f780dd49dd6d5cc6e
Jules Ko DIOUF <jkd.gomycode@gmail.com>
2022-08-30 11:11:47 +0000

    Release 1.0.0

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const App = express()
 
+//Adding template engine pug
 App.set('views', './views')
 App.set('view engine', 'pug')
 
+App.use(timeControl)
+
+// Routes
 App.get('/', (request,response) => {
     response.render('index')
 })
@@ -18,9 +22,25 @@ App.get('/contact', (request,response) => {
 })
 
 App.get('*', (request,response) => {
-    response.end('NOT FOUND !')
+    response.end('404 NOT FOUND !')
 })
 
+//Time Controller middleware
+function timeControl(request,response,next){
+
+    let time = new Date()
+
+    if(time.getDay() !== 0 && time.getDay() != 6){
+        if (time.getHours() > 9 && time.getHours() < 17 )
+            next()
+        else {
+            response.end("L'application est fermée !")
+            console.log("L'application est down !")
+        }
+    }
+}
+
+//Server listening port
 App.listen(3000, () => {
     console.log('Server running on port 3000 ...')
 })
